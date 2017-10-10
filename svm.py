484cd25361c1b9153385e0069b624d8502b16fab
Iao Seng Sio <tonysio42@gmail.com>
2017-10-09 19:03:33 -1000

    test

--- svm.py
+++ svm.py
@@ -6,6 +6,16 @@ plots_directory = ""
 plt.rcParams['figure.figsize'] = [12, 8]
 plt.rcParams['font.size'] = 25
 
+array = []
+with open('test1.txt', 'r') as f:
+    for line in f:
+        array.append(line)
+
+for line in array:
+    line = line.split('\t')
+
+print(array)
+
 points = np.array([[-1, 0], [1, 0], [2, -1]])
 labels = np.array([-1, 1, 1])
 
@@ -34,8 +44,8 @@ number_of_dimensions = 2
 points = np.random.random(size = (number_of_points, number_of_dimensions))
 labels = np.where(points[:, 0] > points[:, 1], 1, -1)
 
-#plt.scatter(points[:, 0], points[:, 1], s=100, c=["blue" if label == 1 else "red" for label in labels])
-#plt.show()
+# plt.scatter(points[:, 0], points[:, 1], s=100, c=["blue" if label == 1 else "red" for label in labels])
+# plt.show()
 
 our_linear_svm = svm.SVC(kernel = 'linear', C=1.0).fit(points, labels)
 print('Support vectors: ' + str(our_linear_svm.support_vectors_))
@@ -49,3 +59,14 @@ new_points = np.array([[0.5, 0.4], [0.4, 0.5], [0.6, 0.3]])
 print('New points: ' + str(new_points))
 print('Decision function (distance to separating hyperplane) of new points: ' + str(our_linear_svm.decision_function(new_points)))
 print('Predicted values of new points: ' + str(our_linear_svm.predict(new_points)))
+print()
+
+points = np.array([[-1, 0], [0, 0], [1, 0]])
+labels = [1, -1, 1]
+
+# plt.scatter(points[:, 0], points[:, 1], s=100, c=["blue" if label == 1 else "red" for label in labels])
+# plt.show()
+
+our_poly_svm_list = [svm.SVC(kernel = 'poly', degree = 2, C = C) for C in [0.01, 0.99, 1., 1.01, 10, 1e2, 1e6]]
+[o.fit(points, labels) for o in our_poly_svm_list]
+print([o.predict(points) for o in our_poly_svm_list])
