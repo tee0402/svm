3b6a8080792c703b4447d61de1fbd7af9a936985
Iao Seng Sio <tonysio42@gmail.com>
2017-11-26 21:02:34 -1000

    Random stock picker

--- svm.py
+++ svm.py
@@ -1,88 +1,74 @@
-from sklearn import svm
-import numpy as np
+from matplotlib.colors import ListedColormap
 import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.svm import SVC
 
-plots_directory = ""
-plt.rcParams['figure.figsize'] = [12, 8]
-plt.rcParams['font.size'] = 25
 
-array = []
-with open('test1.txt', 'r') as f:
-    for line in f:
-        array.append(line.split())
+def random_stocks(N=10):
+    stocks = []
+    with open('stocks.txt') as file:
+        for line in file:
+            stocks.append(line)
+    return stocks
 
-new_array = []
-for line in array:
-    new_line = []
-    for num in line:
-        new_line.append(float(num))
-    new_array.append(new_line)
 
-N = 10
-array = np.array(new_array[:N])
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
 
-points = np.array(array[:,[0,1]])
-labels = np.array(array[:,2])
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
 
-# print('Points: ' + str(points))
-# print('Labels: ' + str(labels))
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
+                    alpha=0.8, c=cmap(idx),
+                    marker=markers[idx], label=cl)
 
-our_linear_svm = svm.SVC(kernel = 'linear', C=100).fit(points, labels)
-xy_max = 10
+    X_test, y_test = X[test_idx, :], y[test_idx]
 
-x = np.linspace(-1. * xy_max, 1. * xy_max, N)
-y = np.linspace(-1. * xy_max, 1. * xy_max, N)
+    plt.scatter(X_test[:, 0], X_test[:, 1], c='', alpha=1.0, linewidths=1, marker='o', s=55, label='test set')
 
-X,Y = np.meshgrid(x,y)
 
-Z = our_linear_svm.decision_function(np.c_[X.ravel(), Y.ravel()])
-Z = Z.reshape(X.shape)
+print(random_stocks())
+np.random.seed(0)
+X_xor = np.random.randn(200, 2)
+y_xor = np.logical_xor(X_xor[:, 0] > 0,
+                       X_xor[:, 1] > 0)
+y_xor = np.where(y_xor, 1, -1)
 
-plt.contour(X, Y, Z, [0], colors = 'black', linewidths = 2)
-plt.scatter(points[:, 0], points[:, 1], s=100, c=['blue' if label == 1 else 'red' for label in labels])
+plt.scatter(X_xor[y_xor == 1, 0],
+            X_xor[y_xor == 1, 1],
+            c='b', marker='x',
+            label='1')
+plt.scatter(X_xor[y_xor == -1, 0],
+            X_xor[y_xor == -1, 1],
+            c='r',
+            marker='s',
+            label='-1')
+
+plt.xlim([-3, 3])
+plt.ylim([-3, 3])
+plt.legend(loc='best')
+plt.tight_layout()
 plt.show()
 
-# print('Support vectors: ' + str(our_linear_svm.support_vectors_))
-# print('Dual coefficients: ' + str(our_linear_svm.dual_coef_))
-#
-# w = sum((our_linear_svm.dual_coef_ * our_linear_svm.support_vectors_.transpose()).transpose())
-# print('w vector: ' + str(w))
-#
-# new_points = np.array([[2, 3], [3, -2], [5, 28], [-3, -4], [-7, 11], [-4, 0]])
-# print('New points: ' + str(new_points))
-# print('Decision function (distance to separating hyperplane) of new points: ' + str(our_linear_svm.decision_function(new_points)))
-# print('Predicted values of new points: ' + str(our_linear_svm.predict(new_points)))
-# print()
-#
-# number_of_points = 100
-# number_of_dimensions = 2
-#
-# points = np.random.random(size = (number_of_points, number_of_dimensions))
-# labels = np.where(points[:, 0] > points[:, 1], 1, -1)
-#
-# # plt.scatter(points[:, 0], points[:, 1], s=100, c=["blue" if label == 1 else "red" for label in labels])
-# # plt.show()
-#
-# our_linear_svm = svm.SVC(kernel = 'linear', C=1.0).fit(points, labels)
-# print('Support vectors: ' + str(our_linear_svm.support_vectors_))
-# print('Dual coefficients: ' + str(our_linear_svm.dual_coef_))
-#
-# w = sum((our_linear_svm.dual_coef_ * our_linear_svm.support_vectors_.transpose()).transpose())
-# print('w vector: ' + str(w))
-# print('b: ' + str(our_linear_svm.intercept_))
-#
-# new_points = np.array([[0.5, 0.4], [0.4, 0.5], [0.6, 0.3]])
-# print('New points: ' + str(new_points))
-# print('Decision function (distance to separating hyperplane) of new points: ' + str(our_linear_svm.decision_function(new_points)))
-# print('Predicted values of new points: ' + str(our_linear_svm.predict(new_points)))
-# print()
-#
-# points = np.array([[-1, 0], [0, 0], [1, 0]])
-# labels = [1, -1, 1]
-#
-# # plt.scatter(points[:, 0], points[:, 1], s=100, c=["blue" if label == 1 else "red" for label in labels])
-# # plt.show()
-#
-# our_poly_svm_list = [svm.SVC(kernel = 'poly', degree = 2, C = C) for C in [0.01, 0.99, 1., 1.01, 10, 1e2, 1e6]]
-# [o.fit(points, labels) for o in our_poly_svm_list]
-# print([o.predict(points) for o in our_poly_svm_list])
+# Create a SVC classifier using an RBF kernel
+svm = SVC(kernel='rbf', random_state=0, gamma=.01, C=1)
+# Train the classifier
+svm.fit(X_xor, y_xor)
+
+# Visualize the decision boundaries
+plot_decision_regions(X_xor, y_xor, classifier=svm)
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
